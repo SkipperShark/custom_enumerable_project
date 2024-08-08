9de4f8f423ad8fba91d185113b98811ef947d02a
sharman <sharmanfaridz@gmail.com>
2024-08-08 20:08:16 +0800

    complete all but last 2 exercises

--- Gemfile
+++ Gemfile
@@ -1,3 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rspec', '~> 3.10'
+
+gem "solargraph", "~> 0.50.0"
+
+gem "rubocop", "~> 1.65"
--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,6 +1,63 @@
 module Enumerable
   # Your code goes here
+  def my_each
+    for ele in self do
+      yield ele
+    end
+    self
+  end
+
+  def my_all?
+    for ele in self do
+      return false unless yield ele
+    end
+    true
+  end
+
+  def my_any?
+    for ele in self do
+      return true if yield ele
+    end
+    false
+  end
+
+  def my_count
+    if block_given?
+      reduce(0) do |sum, ele|
+        sum += 1 if yield ele
+        sum
+      end
+    else
+      size
+    end
+  end
+
+  def my_each_with_index
+    index = 0
+    for ele in self do
+      yield(ele, index)
+      index += 1
+    end
+  end
+
+  def my_inject(initial_value)
+    result = initial_value
+    for ele in self
+      result = yield(result, ele)
+    end
+    result
+  end
+
+  def my_map
+    arr = []
+    each do |ele|
+      arr << yield(ele)
+    end
+    arr
+  end
 end
+
+
 
 # You will first have to define my_each
 # on the Array class. Methods defined in
